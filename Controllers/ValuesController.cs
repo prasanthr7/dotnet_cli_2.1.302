c0f9062939ffcecfa4e043b24f194bf83ade7a1c
Prasanth Ramachandran <prasanth7@outlook.com>
2018-08-14 00:33:22 +0530

    Add files via upload
    Copied base files

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace Dotnet_cli_2._1._302.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+      
+
+        public ValuesController(){
+            Log.Logger=new LoggerConfiguration()
+            .WriteTo.File("Core_App_Log.log")
+            .CreateLogger();
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            Log.Information("Sending json values");
+            return new string[] { "value1", "value2" };
+           
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public ActionResult<string> Delete(int id)
+        {
+            return BadRequest();
+        }
+    }
+}
